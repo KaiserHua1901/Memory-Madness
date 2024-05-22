0702a89fef26778fa2aed3db24d3ccf735f94126
KaiserHua1901 <KaiserHua1901@outlook.com>
2024-05-21 20:37:58 -0700

    Adding a list of item to dict

---
+++ autoload/image_manager.gd
@@ -0,0 +1,33 @@
+extends Node
+
+var item_images:Array = []
+
+func _ready():
+	load_items_image() 
+	print(item_images)
+
+func add_files_to_list(file_name:String, path:String):
+	var full_path = path + "/" + file_name
+	var dict = {
+		"item": file_name.rstrip(".png"),
+		"item_texture": full_path
+	}
+	item_images.append(dict)
+
+
+
+	
+func load_items_image():
+	var path = "res://assets/glitch"
+	var dir = DirAccess.open(path)
+	var image_files = dir.get_files()
+	# var image_name = dir.list_dir_begin()
+
+	for file_name in image_files:
+		if not file_name.ends_with(".import"):
+			add_files_to_list(file_name, path)
+  
+	
+  
+
+
