d65bb30318aeff25a6d99faadc13551f19e6c39a
KaiserHua1901 <KaiserHua1901@outlook.com>
2024-05-29 14:28:43 -0700

    finish the tween and add rand image

--- autoload/image_manager.gd
+++ autoload/image_manager.gd
@@ -4,13 +4,13 @@ var item_images:Array = []
 
 func _ready():
 	load_items_image() 
-
+	print(item_images)
 
 func add_files_to_list(file_name:String, path:String):
 	var full_path = path + "/" + file_name
 	var dict = {
 		"item": file_name.rstrip(".png"),
-		"item_texture": full_path
+		"item_texture": load(full_path)
 	}
 	item_images.append(dict)
 
@@ -21,14 +21,14 @@ func load_items_image():
 	var path = "res://assets/glitch"
 	var dir = DirAccess.open(path)
 	var image_files = dir.get_files()
-	print(image_files)
 	# var image_name = dir.list_dir_begin()
 
 	for file_name in image_files:
 		if not file_name.ends_with(".import"):
 			add_files_to_list(file_name, path)
-  
-	
+
+func set_rand_image()-> Dictionary:
+	return item_images.pick_random()
   
 
 
--- scenes/Sprites.gd
+++ scenes/Sprites.gd
@@ -1,14 +1,29 @@
 extends TextureRect
 
-const SCALE_NORMAL:Vector2 = Vector2(0.1, 0.1)
-const SCALE_DOWN:Vector2 = Vector2(1.0, 1.0)
-const SCALE_TIMER:float = 0.5
+const SCALE_NORMAL:Vector2 = Vector2(1.0, 1.0)
+const SCALE_DOWN:Vector2 = Vector2(0.1, 0.1)
+const SCALE_TIMER:float = 0.6
+const SCALE_SPIN: Vector2 = Vector2(1.0, 2.0) 
 
 func _ready():
 	run_tween()
 
+func get_rand_image():
+	texture = ImageManager.set_rand_image().item_texture
+
+
+func get_rando_spin_direction():
+	return randf_range(SCALE_SPIN.x, SCALE_SPIN.y) 
+func get_rando_rotation():
+	return deg_to_rad(randf_range(-360,360))
+
+
 func run_tween() -> void: 
 	var tween = get_tree().create_tween()
 	tween.set_loops()
+	tween.tween_callback(get_rand_image)
 	tween.tween_property(self, "scale",SCALE_NORMAL, SCALE_TIMER)
+	tween.tween_property(self, "rotation", get_rando_rotation(),get_rando_spin_direction())
+	
 	tween.tween_property(self, "scale",SCALE_DOWN, SCALE_TIMER)
+	tween.tween_property(self, "rotation", get_rando_rotation(),get_rando_spin_direction())
